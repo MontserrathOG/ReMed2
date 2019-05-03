a26fe9c3e791a8c99704ce97fb2d32c0ca4dd546
MontserrathOG <lightmon16@gmail.com>
2019-05-03 00:10:02 -0500

    primer commit

---
+++ src/main/java/com/example/montse/remed/registrarse.java
@@ -0,0 +1,13 @@
+package com.example.montse.remed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class registrarse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrarse);
+    }
+}
